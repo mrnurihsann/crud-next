af7485fedf5fd4b19b36e09de5630e37e063204c
mrnurihsann <muhammadridhwannurihsan@gmail.com>
2024-07-24 23:00:57 +0700

    update crud

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
-export default function Home() {
-  return <h1 className="title text-2xl">HomePage</h1>;
-}
+import ContactTable from "@/components/contact-table";
+import Search from "@/components/search";
+import Pagination from "@/components/pagination";
+import Footer from "@/components/footer";
+import { Button } from "@/components/buttons";
+import { getContactPages } from "@/lib/data";
+import { Suspense } from "react";
+import { TableSkeleton } from "@/components/skeleton";
+
+const Contact = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await getContactPages(query);
+
+  return (
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <Search />
+        <Button />
+      </div>
+      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+        <ContactTable query={query} currentPage={currentPage} />
+      </Suspense>
+
+      <div className="flex justify-center mt-4">
+        <Pagination totalPages={totalPages} />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
